ae93faf887580dbbdaffee84b4f02a4d218edff0
angela <angelaguacaran@gmail.com>
2023-05-01 22:15:37 -0300

    card

--- tarefa-2/src/Components/Card/Card.jsx
+++ tarefa-2/src/Components/Card/Card.jsx
@@ -7,7 +7,7 @@ function Card(props){
         <div id="main">
             <div id="avaliacao">
                 <div id="imagem">
-                    <img src={props.imagem} alt="imagem"  />
+                    <img src={props.imagem} alt="imagem" width={70} />
                 </div>
                 <div id="texto">
                     <h1>{props.titulo}</h1>
